f4927c13c0dd098300281039357b9d4e5f421752
Marv0912 <marvin.fajardo.s@icloud.com>
2023-12-11 15:06:24 -0400

    sidebar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-
-
+import Sidebar from "./components/sidebar/Sidebar";
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+  <Sidebar />
+    {/* <Routes>
+      <Route>
+
+      </Route>
+    </Routes> */}
+
+  </div>;
 }
 
 export default App;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Sidebar.css'
 
 const Sidebar = () => {
   return (
-    <div>
-      
+    <div className="sidebar-container">
+      <nav>
+        <ul className="nav-list">
+          <li>
+            <NavLink to="/" className="nav-link">
+              + Create a new task
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/" className="nav-link">
+              To-Do
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/" className="nav-link">
+              In-Progress
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/" className="nav-link">
+              Completed
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
-}
+};
 
 export default Sidebar;
