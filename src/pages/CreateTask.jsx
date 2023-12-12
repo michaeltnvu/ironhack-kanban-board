e3cf0b04cc5dac82434ab26e99e4875ee65b8cb2
Michael Vu <vu.michaeltien@gmail.com>
2023-12-12 13:14:44 -0500

    Add Link component in Footer.jsx | Removed redudant list items in Sidebar.jsx | Generated unique id for new tasks

--- src/pages/CreateTask.jsx
+++ src/pages/CreateTask.jsx
@@ -1,98 +1,96 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
 const CreateTask = ({ addTask, tasks }) => {
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [assignee, setAssignee] = useState("");
-    const [status, setStatus] = useState("To-Do");
-    const [priority, setPriority] = useState("Medium");
-    const [dueDate, setDueDate] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [assignee, setAssignee] = useState("");
+  const [status, setStatus] = useState("To-Do");
+  const [priority, setPriority] = useState("Medium");
+  const [dueDate, setDueDate] = useState("");
 
-    const navigate = useNavigate();
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newTask = {
-            id: (tasks.length + 1).toString(), // changing comment to commit
-            title,
-            description,
-            assignee,
-            status,
-            priority,
-            createdDate: new Date().toISOString().split('T')[0], // Current date in ISO format
-            dueDate
-        };
-        addTask(newTask); // Function to add the new task to the list
-        console.log(`task created ${newTask}`)
+  function generateUniqueId() {
+    return Math.random().toString(36).slice(2, 11) + Date.now().toString(36);
+  }
 
-        navigate('/');
-        // Reset form fields
-        setTitle('');
-        setDescription('');
-        setAssignee('');
-        setStatus('To Do');
-        setPriority('Medium');
-        setDueDate('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = {
+      id: generateUniqueId(), // changing comment to commit
+      title,
+      description,
+      assignee,
+      status,
+      priority,
+      createdDate: new Date().toISOString().split("T")[0], // Current date in ISO format
+      dueDate,
     };
-    
+    addTask(newTask); // Function to add the new task to the list
+    navigate("/");
+    setTitle("");
+    setDescription("");
+    setAssignee("");
+    setStatus("To Do");
+    setPriority("Medium");
+    setDueDate("");
+  };
 
-
-return (
+  return (
     <form onSubmit={handleSubmit}>
-        <div>
-            <label>Title:</label>
-            <input
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-            />
-        </div>
-        <div>
-            <label>Description:</label>
-            <textarea
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-            ></textarea>
-        </div>
-        <div>
-            <label>Assignee:</label>
-            <input
-                type="text"
-                value={assignee}
-                onChange={(e) => setAssignee(e.target.value)}
-            />
-        </div>
-        <div>
-            <label>Status:</label>
-            <select value={status} onChange={(e) => setStatus(e.target.value)}>
-                <option value="To-Do">To-Do</option>
-                <option value="In-Progress">In-Progress</option>
-                <option value="Completed">Completed</option>
-            </select>
-        </div>
-        <div>
-            <label>Priority:</label>
-            <select value={priority} onChange={(e) => setPriority(e.target.value)}>
-                <option value="High">High</option>
-                <option value="Medium">Medium</option>
-                <option value="Low">Low</option>
-            </select>
-        </div>
-        <div>
-            <label>Due Date:</label>
-            <input
-                type="date"
-                value={dueDate}
-                onChange={(e) => setDueDate(e.target.value)}
-            />
-        </div>
-        <Link to={"/"}>
-            <button>Back</button>
-        </Link>
-        <button type="submit">Add Task</button>
+      <div>
+        <label>Title:</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Description:</label>
+        <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+      </div>
+      <div>
+        <label>Assignee:</label>
+        <input
+          type="text"
+          value={assignee}
+          onChange={(e) => setAssignee(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Status:</label>
+        <select value={status} onChange={(e) => setStatus(e.target.value)}>
+          <option value="To-Do">To-Do</option>
+          <option value="In-Progress">In-Progress</option>
+          <option value="Completed">Completed</option>
+        </select>
+      </div>
+      <div>
+        <label>Priority:</label>
+        <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+          <option value="High">High</option>
+          <option value="Medium">Medium</option>
+          <option value="Low">Low</option>
+        </select>
+      </div>
+      <div>
+        <label>Due Date:</label>
+        <input
+          type="date"
+          value={dueDate}
+          onChange={(e) => setDueDate(e.target.value)}
+        />
+      </div>
+      <Link to={"/"}>
+        <button>Back</button>
+      </Link>
+      <button type="submit">Add Task</button>
     </form>
-);
+  );
 };
 
 export default CreateTask;
